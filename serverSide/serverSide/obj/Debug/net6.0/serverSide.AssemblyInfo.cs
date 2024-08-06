6ddf11a07094969a6d42fe4243e1d4a1ed4e719d
OfriG <ofri900@gmail.com>
2024-08-06 16:38:59 +0300

    oo

--- serverSide/serverSide/obj/Debug/net6.0/serverSide.AssemblyInfo.cs
+++ serverSide/serverSide/obj/Debug/net6.0/serverSide.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("serverSide")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a16b56ff72bc22a17122c67e6e234f519d2bb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("serverSide")]
 [assembly: System.Reflection.AssemblyTitleAttribute("serverSide")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
