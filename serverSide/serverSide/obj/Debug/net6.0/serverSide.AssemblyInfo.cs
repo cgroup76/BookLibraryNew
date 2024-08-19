ff90c160205f1dc15c9aa7571f9f7593d4962b0b
AdiDagan1 <adidagan2271@gmail.com>
2024-08-19 14:08:51 +0300

    ignore

--- serverSide/serverSide/obj/Debug/net6.0/serverSide.AssemblyInfo.cs
+++ serverSide/serverSide/obj/Debug/net6.0/serverSide.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("serverSide")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0504a6096914f986fcc41b054bac916cf855abf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("serverSide")]
 [assembly: System.Reflection.AssemblyTitleAttribute("serverSide")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
